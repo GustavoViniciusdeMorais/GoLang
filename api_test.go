96e22b15bdb8a6f3cb876eb6ee4efc5e4044e289
GustavoViniciusdeMorais <gustavocop14dos@gmail.com>
2024-06-08 12:25:32 -0300

    - Starting nginx configuration

---
+++ api_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", helloHandler)
+	log.Println("Starting server on :8080...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf("Could not start server: %s\n", err)
+	}
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, World!")
+}
