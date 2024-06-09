184336839a2487fa2cb5ddbecd675ef32f85278e
GustavoViniciusdeMorais <gustavocop14dos@gmail.com>
2024-06-09 11:56:16 -0300

    - Fixed the api test

--- apitest.go
+++ apitest.go
@@ -7,13 +7,13 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/", check)
 	log.Println("Starting server on :8080...")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatalf("Could not start server: %s\n", err)
 	}
 }
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, World!")
+func check(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "It is alive!")
 }
--- ubuntu.dockerfile
+++ ubuntu.dockerfile
@@ -1,8 +1,8 @@
-FROM gustavovinicius/golang:latest
+FROM gustavovinicius/golang:nginx
 # FROM grpccplus:latest
 # RUN rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.0.linux-amd64.tar.gz
 # RUN apt update
 # RUN apt install g++
 # RUN apt install build-essential
-RUN export PATH=$PATH:/usr/local/go/bin
+# RUN export PATH=$PATH:/usr/local/go/bin
 ENTRYPOINT ["tail", "-f", "/dev/null"]
